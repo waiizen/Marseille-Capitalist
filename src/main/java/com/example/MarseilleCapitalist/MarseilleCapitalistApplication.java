c7c697b052af9bb56f719f3cc32f56297fa34ed4
Waiizen <nathanferrer.nf@gmail.com>
2021-06-08 09:27:25 +0200

    Added spring skeleton

---
+++ src/main/java/com/example/MarseilleCapitalist/MarseilleCapitalistApplication.java
@@ -0,0 +1,13 @@
+package com.example.MarseilleCapitalist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarseilleCapitalistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarseilleCapitalistApplication.class, args);
+	}
+
+}
