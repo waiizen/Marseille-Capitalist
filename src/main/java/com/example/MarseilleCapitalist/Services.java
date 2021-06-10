30bb0ee620f6e8ed877f823cf0f14f81a3eb28be
Guillaume <guillaume.grazioli@etud.univ-jfc.fr>
2021-06-10 09:15:04 +0200

    commit

--- src/main/java/com/example/MarseilleCapitalist/Services.java
+++ src/main/java/com/example/MarseilleCapitalist/Services.java
@@ -1,16 +1,37 @@
 package com.example.MarseilleCapitalist;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import generated.World;
+import java.io.File;
+
 public class Services {
 
-    public generated.World readWorldFromXml(){
-        //TODO
+    File xmlFile = new File("worldSchema.xml");
+    JAXBContext jaxbContext;
+
+    public World readWorldFromXml(){
+        try
+        {
+            jaxbContext = JAXBContext.newInstance(World.class);
+
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+            generated.World employee = (World) jaxbUnmarshaller.unmarshal(xmlFile);
+
+            System.out.println(employee);
+        }
+        catch (JAXBException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public void saveWorldToXml(generated.World world){
         //TODO
     }
 
-    public generated.World getWorld(){
+    public World getWorld(){
         //TODO
     }
 
