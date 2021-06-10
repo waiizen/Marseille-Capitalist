454df1befb41b60e4fe7a1f5e5ea71b88f8fafec
Waiizen <nathanferrer.nf@gmail.com>
2021-06-10 08:52:07 +0200

    Update

---
+++ src/main/java/com/example/MarseilleCapitalist/Services.java
@@ -0,0 +1,17 @@
+package com.example.MarseilleCapitalist;
+
+public class Services {
+
+    public generated.World readWorldFromXml(){
+        //TODO
+    }
+
+    public void saveWorldToXml(generated.World world){
+        //TODO
+    }
+
+    public generated.World getWorld(){
+        //TODO
+    }
+
+}
